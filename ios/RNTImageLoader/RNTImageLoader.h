4847965dcd3eb5a2174d68dbd2633f0bd7e2690f
musicode <musicode@qq.com>
2020-07-10 20:38:11 +0800

    update

--- ios/RNTImageLoader/RNTImageLoader.h
+++ ios/RNTImageLoader/RNTImageLoader.h
@@ -6,9 +6,9 @@
 
 + (void)loadImage:(NSString *)url onProgress:(void (^)(NSInteger, NSInteger))onProgress onComplete:(void (^)(UIImage*))onComplete;
 
-+ (void)setThumbnailImage:(UIImageView *)imageView url:(NSString *)url placeholder:(UIImage *)placeholder onProgress:(void (^)(NSInteger, NSInteger))onProgress onComplete:(void (^)(UIImage*))onComplete;
++ (void)setThumbnailImage:(UIImageView *)imageView url:(NSString *)url loading:(UIImage *)loading error:(UIImage *)error onProgress:(void (^)(NSInteger, NSInteger))onProgress onComplete:(void (^)(UIImage*))onComplete;
 
-+ (void)setHDImage:(UIImageView *)imageView url:(NSString *)url placeholder:(UIImage *)placeholder onProgress:(void (^)(NSInteger, NSInteger))onProgress onComplete:(void (^)(UIImage*))onComplete;
++ (void)setHDImage:(UIImageView *)imageView url:(NSString *)url loading:(UIImage *)loading error:(UIImage *)error onProgress:(void (^)(NSInteger, NSInteger))onProgress onComplete:(void (^)(UIImage*))onComplete;
 
 + (NSString *)getImageCachePath:(NSString *)url;
 
