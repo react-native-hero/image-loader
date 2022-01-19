3367ebc1caba5f74c5321797ca696841a73d6b90
musicode <musicode@qq.com>
2022-01-19 10:44:57 +0800

    bump 0.1.0

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { RNTImageLoader } = NativeModules
 /**
  * 把 base64 图片保存到本地
  */
- export function saveBase64Image(options) {
+export function saveBase64Image(options) {
+  options.base64 = options.base64.replace(/^data:image\/[^;]+;base64,/, '')
   return RNTImageLoader.saveBase64Image(options)
 }
